a570bcd8b678b1d8101a43f5dd010b4830f4a6e5
soofigonb <sofiapazgonb@gmail.com>
2024-11-06 17:10:21 -0300

    Agregando carpeta src que contiene la api.js y el array  pokemon.js

---
+++ src/api/api.js
@@ -0,0 +1,5 @@
+const api = {
+    pokemonApi: 'https://pokeapi.co/api/v2/pokemon/'
+};
+
+export default api;
---
+++ src/pokemon/pokemon.js
@@ -0,0 +1,8 @@
+const pokemonList = [
+    "bulbasaur", "ivysaur", "venusaur", "squirtle", 
+    "wartortle", "blastoise", "charmander", "charmeleon", "charizard"
+];
+
+export default pokemonList;
+
+
